0809805d659a478dfd4c98ffe924fbd30d4d7562
KiSae-Lee <kisaelee.github@gmail.com>
2022-10-19 17:47:08 +0900

    Add UI template.

---
+++ Source/UE5_Template/UE5_TemplateGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5_TemplateGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_TEMPLATE_API AUE5_TemplateGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
